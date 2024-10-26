38325fc8163527192c231cdce7e4afddb1e158b1
satwikShresth <satwikshresth@gmail.com>
2024-10-26 12:58:11 -0400

    adding docker inital files

---
+++ fullstack/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ENV_JSON_PATH="/fullstack/env.json"
+
+cat <<EOF > $ENV_JSON_PATH
+{
+  "user": "${POSTGRES_USER}",
+  "password": "${POSTGRES_PASSWORD}",
+  "host": "${POSTGRES_SERVER}",
+  "database": "${POSTGRES_DB}",
+  "port": "${POSTGRES_PORT}"
+}
+EOF
+
+# Execute the original command
+exec "$@"
+
+cat $ENV_JSON_PATH
+
+
+if [ ${ENV} = "DEV" ]; then 
+  deno fmt --watch &
+  deno task dev
+else
+  deno task start
+fi
