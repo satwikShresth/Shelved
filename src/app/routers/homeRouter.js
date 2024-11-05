afc3bba4a6e096becceb52aeaaa936c7de0174c2
satwikShresth <satwikshresth@gmail.com>
2024-11-05 14:22:36 -0500

    makeing homepage using ejs

--- src/app/routers/homeRouter.js
+++ src/app/routers/homeRouter.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import tmdbService from "services/tmdbService.js";
 
 const getHomeRouter = () => {
   const router = Router();
 
-  router.get("/homepage", (_req, res) => {
-    res.render("homepage", { username: res.locals.username });
+  router.get("/homepage", async (_req, res) => {
+    const trendingData = await tmdbService
+      .getTrending({ range: "week", mediaType: "all" })
+      .then((data) => {
+        return data.results.slice(0, 5);
+      })
+      .catch((error) => {
+        console.error("Error fetching trending data:", error.message);
+        res.status(500).json({ error: "Failed to fetch trending data" });
+      });
+
+    res.render("homepage", {
+      username: res.locals.username,
+      trendingData: trendingData,
+    });
   });
 
   router.get("/whoami", (_req, res) => {
