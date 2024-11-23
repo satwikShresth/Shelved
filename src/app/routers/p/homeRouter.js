cad520e3591813e48aad9fd51fe25bd84ad6bb6f
Zach Thomas <zachthomas2025@gmail.com>
2024-11-23 13:07:16 -0500

    Combined homepage endpoint with trending functionality

--- src/app/routers/p/homeRouter.js
+++ src/app/routers/p/homeRouter.js
@@ -3,6 +3,7 @@ import { getVisibilityOptions } from "crud/visibility.js";
 import { getShelvesByUserId } from "crud/shelf.js";
 import services from "services/index.js";
 import { getDetailedShelfContent } from "middlewares/tmdbMiddleware.js";
+import axios from 'axios';
 
 const getHomeRouter = () => {
   const router = Router();
@@ -16,19 +17,66 @@ const getHomeRouter = () => {
     }
 
     const tmdbService = services["tmdb"];
-
-    const tmdbTrending = await tmdbService
-      .getTrending({ range: "week", mediaType: "all" })
-      .then((data) => data.results)
+    
+    const moviesData = await tmdbService
+      .getTrending({ range: 'week', mediaType: 'movie' })
       .catch((error) => {
-        console.error("Error fetching TMDB trending data:", error.message);
-        return [];
+        console.error('Error fetching trending data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch trending data' });
       });
+    const showsData = await tmdbService
+      .getTrending({ range: 'week', mediaType: 'tv' })
+      .catch((error) => {
+        console.error('Error fetching trending data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch trending data' });
+      });
+    const booksData = await axios
+      .request(
+        `https://openlibrary.org/search.json?q=subject:fiction&sort=readinglog&limit=5&fields=title,author_name,cover_i,first_publish_year,ratings_average`
+      )
+      .catch((err) => console.error(err))
+
+    let trendingMovies = [];
+    let trendingShows = [];
+    let trendingBooks = [];
+    for (let i = 0; i < 5; i++) {
+      let movie = moviesData.results[i];
+      trendingMovies.push({
+        title: movie.title,
+        release_date: movie.release_date,
+        overview: movie.overview,
+        vote_average: movie.vote_average,
+        poster: `https://image.tmdb.org/t/p/original${movie.poster_path}`,
+        media_type: 'movie',
+      });
+
+      let show = showsData.results[i];
+      trendingShows.push({
+        title: show.name,
+        release_date: show.first_air_date,
+        overview: movie.overview,
+        vote_average: movie.vote_average,
+        poster: `https://image.tmdb.org/t/p/original${show.poster_path}`,
+        media_type: 'tv',
+      });
+
+      let book = booksData.data.docs[i];
+      trendingBooks.push({
+        title: book.title,
+        release_date: book.first_publish_year.toString(),
+        overview: book.author_name[0],
+        vote_average: book.ratings_average,
+        poster: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
+        media_type: 'book',
+      });
+    }
 
     res.render("homepage", {
       username: res.locals.username,
       trendingData: {
-        tmdb: tmdbTrending,
+        movies: trendingMovies,
+        shows: trendingShows,
+        books: trendingBooks
       },
       shelves: shelvesResponse.shelves || [],
     });
@@ -67,6 +115,67 @@ const getHomeRouter = () => {
   router.get("/whoami", (_req, res) => {
     res.send(`Hello user: ${res.locals.username}`);
   });
+
+  router.get(
+    '/trending',
+    async (_req, res) => {
+      let trendingMovies = [];
+      let trendingShows = [];
+      let trendingBooks = [];
+
+      const tmdbService = services["tmdb"];
+
+      const moviesData = await tmdbService
+        .getTrending({ range: 'week', mediaType: 'movie' })
+        .catch((error) => {
+          console.error('Error fetching trending data:', error.message);
+          res.status(500).json({ error: 'Failed to fetch trending data' });
+        });
+      const showsData = await tmdbService
+        .getTrending({ range: 'week', mediaType: 'tv' })
+        .catch((error) => {
+          console.error('Error fetching trending data:', error.message);
+          res.status(500).json({ error: 'Failed to fetch trending data' });
+        });
+      const booksData = await axios
+        .request(
+          `https://openlibrary.org/search.json?q=subject:fiction&sort=readinglog&limit=5&fields=title,author_name,cover_i,first_publish_year`
+        )
+        .catch((err) => console.error(err))
+
+      for (let i = 0; i < 5; i++) {
+        let movie = moviesData.results[i];
+        trendingMovies.push({
+          title: movie.title,
+          release_date: movie.release_date,
+          poster: `https://image.tmdb.org/t/p/original${movie.poster_path}`,
+          media_type: 'movie',
+        });
+
+        let show = showsData.results[i];
+        trendingShows.push({
+          title: show.name,
+          release_date: show.first_air_date,
+          poster: `https://image.tmdb.org/t/p/original${show.poster_path}`,
+          media_type: 'tv',
+        });
+
+        let book = booksData.data.docs[i];
+        trendingBooks.push({
+          title: book.title,
+          author: book.author_name[0],
+          release_date: book.first_publish_year.toString(),
+          poster: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`,
+          media_type: 'book',
+        });
+      }
+      res.json({
+        movies: trendingMovies,
+        shows: trendingShows,
+        books: trendingBooks
+      });
+    }
+  );
 
   return router;
 };
